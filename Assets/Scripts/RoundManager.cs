21c130ecdd07268bf9215005b85310c54862c5c2
Hedi <zitouni.hedi1@gmail.com>
2019-11-12 22:38:38 +0100

    Can go to the next round

---
+++ Assets/Scripts/RoundManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoundManager : MonoBehaviour
+{
+    public int round;
+    private Text textRoundNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        round = 1;
+        textRoundNumber = GameObject.Find("RoundNumber").GetComponent<Text>();
+        displayRound();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void nextRound()
+    {
+        round++;
+        displayRound();
+    }
+
+    public void displayRound()
+    {
+        textRoundNumber.text = "Round : " + round.ToString();
+    }
+}
