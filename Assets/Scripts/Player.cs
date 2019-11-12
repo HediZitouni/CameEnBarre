a8331c90dc28fa736364b1109045708df81cbd46
Hedi <zitouni.hedi1@gmail.com>
2019-11-13 00:08:31 +0100

    start to manage coins and player names

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public int coins;
+    public string playerName;
+    public Text textPlayerName;
+    public Text textPlayerCoin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coins = 0;
+        displayPlayeName();
+        dislayPlayerCoins();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addCoin()
+    {
+        coins++;
+        dislayPlayerCoins();
+    }
+    
+    public void removeCoin()
+    {
+        coins--;
+        dislayPlayerCoins();
+    }
+
+    void displayPlayeName()
+    {
+        textPlayerName.text = playerName;
+    }
+
+    void dislayPlayerCoins()
+    {
+        textPlayerCoin.text = ": " + coins.ToString();
+    }
+}
